b4a87a959d8613199d17e19ee3ceb4d71a223379
seanolson <solson@kipsu.com>
2023-01-04 21:09:57 -0600

    add layout w/ appbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import DrawerAppBar from './Layout/Layout';
+
+// Material UI
+// import MuiThemeProvider from '@mui/material/styles';
+// import createTheme from '@mui/material';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <DrawerAppBar />
+    </div>
+  );
+}
